bb893cd772c7d9090ec64117254190dda0841a76
emir996 <edzinovic96@gmail.com>
2019-12-26 15:40:44 +0100

    configuration file contains params for db and root of app

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+// define application root
+define('APP_DIR', "c:/xampp/htdocs/licenseManager/");
+
+//Define db params
+define('DBHOST',"localhost");
+define('DBUSER',"root");
+define('DBPASS',"");
+define('DBNAME',"license_manager");
+
+
